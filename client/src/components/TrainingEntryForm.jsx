e39a8612e6687d56d035987f1d7d861c16e4182e
Janimatti Ellonen <janimatti.ellonen@fraktio.fi>
2017-09-25 08:51:00 +0300

    added some initial form fields

--- client/src/components/TrainingEntryForm.jsx
+++ client/src/components/TrainingEntryForm.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
-import { Button, Col, Form, FormGroup, Row } from 'react-bootstrap';
+import { Button, Col, ControlLabel, Form, FormGroup, Row } from 'react-bootstrap';
 
 const validate = (values) => {
   const errors = {};
+
+  if (!values.duration) {
+    errors.duration = 'Value is required';
+  }
+  
+  console.log(JSON.stringify(errors));
 
   return errors;
 }
@@ -14,7 +20,31 @@ const TrainingEntryForm = ({ pristine, submitting }) => (
     <Form>
       <Row>
         <Col md={12} sm={12} xs={12}>
-          <p>Training entry form</p>
+          <ControlLabel>Duration</ControlLabel>
+
+          <Field
+            name="duration"
+            component="input"
+            type="text"
+          />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={12} sm={12} xs={12}>
+          <ControlLabel>Precision</ControlLabel>
+
+          <Field
+            name="precision"
+            component="input"
+            type="text"
+          />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={12} sm={12} xs={12}>
+          <Button bsStyle="success" disabled={pristine || submitting} type="submit">Lähetä vastaus</Button>
         </Col>
       </Row>
     </Form>
